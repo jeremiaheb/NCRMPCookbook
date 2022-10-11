67463d063130a7cf8fd6722f2482192656491f5d
kenzieknox <mackenziefknox@gmail.com>
2022-10-11 15:58:54 -0700

    figures populated to pages, removed hist and line graph pages from site

--- docs/fish_plots/theme_publication.R
+++ docs/fish_plots/theme_publication.R
@@ -46,7 +46,7 @@ scale_fill_Publication_BW <- function(...){
   
 }
 
-scale_colour_Publication <- function(...){
+scale_color_Publication <- function(...){
   library(scales)
   discrete_scale("colour","Publication",manual_pal(values = c("#386cb0","#fdb462","#7fc97f","#ef3b2c","#662506","#a6cee3","#fb9a99","#984ea3","#ffff33")), ...)
 
